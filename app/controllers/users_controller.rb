c0588b7431d06d869c7949613b1c13df310ba3e4
Juli Emmel <justjiffy@gmail.com>
2015-09-14 10:34:52 -0700

    susy is installed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-def index
+ def index
  end
 
  def new
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
 
 require "rails"
+require "susy"
+
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
